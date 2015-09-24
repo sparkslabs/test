71809485134334911627af709ef898b1a732973c
Michael Sparks <sparks.m@gmail.com>
2015-09-24 22:44:51 +0100

    Add blog app to settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'blog',
 )
 
 ROOT_URLCONF = 'mysite.urls'
